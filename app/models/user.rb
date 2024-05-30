193c257033b3e5427c7f3bb8d710e04034248c34
W.Y.Chang <solowing31@gmail.com>
2024-05-30 18:25:18 +0800

    Update: setting user model p2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-    include ActiveModel::SecurePassword
     has_secure_password
-    has_secure_password :recovery_password, validations: false
-    attr_accessor :password_digest, :recovery_password_digest
+    validates :email, presence: true
 
     self.primary_key = :id_hash
 end

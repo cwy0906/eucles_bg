4920a748c34930a0b83d29b23bce285eb1665b81
W.Y.Chang <solowing31@gmail.com>
2024-08-27 22:54:49 +0800

    Update: frontend users page frame preparation p3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,24 @@
 class UsersController < ApplicationController
+  after_action :clear_common_message, except: [:create, :destroy]
 
   def index
-    @show_users = User.all
+    @show_users = User.all 
+    session[:common_message] ||= { title: "Notice", content: "welcome to user index page." }
   end
 
   def new
+    session[:common_message] = { title: "Notice", content: "Input new user profile." }
   end
 
   def create
     @reserve_user    = User.new(create_user_params)
     @reserve_user.id = SecureRandom.hex(16)
     
-    @reserve_user.save
+    if @reserve_user.save
+      session[:common_message] = { title: "Notice", content: "New user successfully created." }
+    else
+      session[:common_message]  = { title: "Alert", content: "Failed to create new user." }
+    end
 
     redirect_to users_path
   end
@@ -20,7 +27,11 @@ class UsersController < ApplicationController
     user_id_hash = delete_user_params[:id]
     chose_user   = User.find_by(id_hash: user_id_hash)
 
-    User.find(user_id_hash).destroy
+    if User.find(user_id_hash).destroy
+      session[:common_message] = { title: "Notice", content: "Chose user successfully destroyed." }
+    else
+      session[:common_message]  = { title: "Alert", content: "Failed to destroy chose user." }
+    end
 
     redirect_to users_path
   end
@@ -33,6 +44,10 @@ class UsersController < ApplicationController
 
   def delete_user_params
     params.permit(:id, :_method)
+  end
+
+  def clear_common_message
+    session[:common_message] = nil
   end
 
 end
